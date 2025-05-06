f1189bb283e0722002c252d32f9ad94f653d943d
tangjunjie08 <tangjunjie08@meituan.com>
2025-05-06 17:32:30 +0800

    feat：禁用公告

--- src/core/webview/ClineProvider.ts
+++ src/core/webview/ClineProvider.ts
@@ -1138,7 +1138,6 @@ export class ClineProvider extends EventEmitter<ClineProviderEvents> implements 
 	async getStateToPostToWebview() {
 		const {
 			apiConfiguration,
-			lastShownAnnouncementId,
 			customInstructions,
 			alwaysAllowReadOnly,
 			alwaysAllowReadOnlyOutsideWorkspace,
@@ -1232,8 +1231,7 @@ export class ClineProvider extends EventEmitter<ClineProviderEvents> implements 
 			ttsSpeed: ttsSpeed ?? 1.0,
 			diffEnabled: diffEnabled ?? true,
 			enableCheckpoints: enableCheckpoints ?? true,
-			shouldShowAnnouncement:
-				telemetrySetting !== "unset" && lastShownAnnouncementId !== this.latestAnnouncementId,
+			shouldShowAnnouncement: false,
 			allowedCommands,
 			soundVolume: soundVolume ?? 0.5,
 			browserViewportSize: browserViewportSize ?? "900x600",
